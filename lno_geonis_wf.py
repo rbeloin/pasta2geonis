7ec255a42bafc132814f3ac84910fdbadee1fcf0
rbeloin <ronbeloin+git@gmail.com>
2013-01-15 14:51:09 -0800

    More testing of python toolbox. Created scripts folder.

--- lno_geonis_wf.py
+++ lno_geonis_wf.py
@@ -70,15 +70,15 @@ class testtool(ArcpyTool):
         
     @property
     def label(self):
-        return "test"
+        return "testtool"
     
     @property
     def alias(self):
-        return "test alias"
+        return "testtool alias"
     
     @property
     def description(self):
-        return "test description property"
+        return "test description property or testtool, the original"
     
     def getParameterInfo(self):
         params = []
@@ -91,9 +91,41 @@ class testtool(ArcpyTool):
         return
 
     def execute(self, parameters, messages):
-        return (len(parameters), len(messages))
+        #print("params: " + str(len(parameters)) + ", messages: " + str(len(messages)))
+        messages.addMessage("test 1 done");
+        return
     
+class test2tool(ArcpyTool):
+    def __init__(self):
+        pass
+        
+    @property
+    def label(self):
+        return "test2tool"
     
+    @property
+    def alias(self):
+        return "test2tool alias"
+    
+    @property
+    def description(self):
+        return "this is test 2 tool with changes"
+    
+    def getParameterInfo(self):
+        params = []
+        return params
+
+    def updateParameters(self, parameters):
+        return
+
+    def updateMessages(self, parameters):
+        return
+
+    def execute(self, parameters, messages):
+        #print("params: " + str(len(parameters)) + ", messages: " + str(len(messages)))
+        messages.addMessage("test 2 done");
+        return
+        
 def quicktest():
     mytest = testtool()
     print mytest.canRunInBackground
