16baf22d680f6098983589dfb149dea4d37b8b42
rbeloin <ronbeloin+git@gmail.com>
2013-01-17 13:10:18 -0800

    Created pyscripter project, added script folder to project. We'll see.

--- scripts/lno_geonis_wf.py
+++ scripts/lno_geonis_wf.py
@@ -5,7 +5,7 @@ Created on Jan 14, 2013
 @change: https://github.com/rbeloin/pasta2geonis
 
 @author: ron beloin
-@copyright: 2013 LTER Network Office, University of New Mexico 
+@copyright: 2013 LTER Network Office, University of New Mexico
 @see https://nis.lternet.edu/NIS/
 '''
 import sys, os
@@ -14,24 +14,24 @@ import arcpy
 from lno_geonis_base import ArcpyTool
 from lno_geonis_base import getListofCommonInputs, updateParametersCommon
 
-
+arcpy.AddMessage("nothing")
 
 class testtool(ArcpyTool):
     def __init__(self):
-        pass    
-        
+        pass
+
     @property
     def label(self):
         return "testtool"
-    
+
     @property
     def alias(self):
         return "testtool alias"
-    
+
     @property
     def description(self):
         return "test description property of testtool, the original"
-    
+
     def getParameterInfo(self):
         params = getListofCommonInputs()
         return params
@@ -47,26 +47,26 @@ class testtool(ArcpyTool):
         if(self.isRunningAsTool):
             messages.AddMessage("this message brought to you via message object pass in")
         else:
-            arcpy.AddMessage("this message brought to you via arcpy")            
+            arcpy.AddMessage("this message brought to you via arcpy")
         return
 
-    
+
 class test2tool(ArcpyTool):
     def __init__(self):
         pass
-        
+
     @property
     def label(self):
         return "test2tool"
-    
+
     @property
     def alias(self):
         return "test2tool alias"
-    
+
     @property
     def description(self):
         return "this is test 2 tool with changes"
-    
+
     def getParameterInfo(self):
         params = getListofCommonInputs()
         return params
@@ -81,9 +81,9 @@ class test2tool(ArcpyTool):
         if(self.isRunningAsTool):
             messages.AddMessage("this message brought to you via message object pass in")
         else:
-            arcpy.AddMessage("this message brought to you via arcpy")            
+            arcpy.AddMessage("this message brought to you via arcpy")
         return
- 
+
 
 def quicktest():
     #testtool.runAsToolboxTool()
@@ -95,7 +95,6 @@ def quicktest():
     print mytest.description
     print mytest.getParameterInfo()
     print mytest.execute([], [])
-    
+
 if __name__ == '__main__':
     quicktest()
-    
