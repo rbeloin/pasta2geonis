05f1793fd14ee479794cd0ab5482b9f52c39cab5
rbeloin <ronbeloin+git@gmail.com>
2013-01-18 12:26:46 -0800

    Tools can be connected in model builder. Added log file as optional.

--- scripts/lno_geonis_base.py
+++ scripts/lno_geonis_base.py
@@ -4,13 +4,13 @@ Created on Jan 15, 2013
 @change: https://github.com/rbeloin/pasta2geonis
 
 @author: ron beloin
-@copyright: 2013 LTER Network Office, University of New Mexico 
+@copyright: 2013 LTER Network Office, University of New Mexico
 @see https://nis.lternet.edu/NIS/
 '''
+import sys, os
 import arcpy
 from abc import ABCMeta, abstractmethod, abstractproperty
 from arcpy import Parameter
-
 
 class ArcpyTool:
     """
@@ -21,22 +21,22 @@ class ArcpyTool:
     tool can be used.
     """
     __metaclass__ = ABCMeta
-    
+
     #class level var
     isRunningAsTool = False
-    
+
     @classmethod
     def runAsToolboxTool(cls):
         cls.isRunningAsTool = True
-        
+
     @classmethod
     def isArcpyTool():
         return True
-    
+
     @property
     def canRunInBackground(self):
         return False
-    
+
     def isLicensed(self):
         """Set whether tool is licensed to execute."""
         return True
@@ -44,20 +44,20 @@ class ArcpyTool:
     @abstractproperty
     def description(self):
         pass
-    
+
     @abstractproperty
     def label(self):
         pass
-    
+
     @abstractproperty
     def alias(self):
         pass
-    
+
     @abstractmethod
     def getParameterInfo(self):
         """Define parameter definitions"""
         pass
-    
+
     @abstractmethod
     def updateParameters(self, parameters):
         """Modify the values and properties of parameters before internal
@@ -75,29 +75,66 @@ class ArcpyTool:
     def execute(self, parameters, messages):
         """The source code of the tool."""
         return
-    
+
 def getListofCommonInputs():
     commonparams = [Parameter(
                       displayName = 'Verbose',
-                      name = 'msgs_on',
+                      name = 'send_msgs',
                       datatype = 'Boolean',
                       direction = 'Input',
                       parameterType = 'Optional'),
                     Parameter(
-                      displayName = 'Log file location',
+                      displayName = 'Log file',
                       name = 'logfilepath',
-                      datatype = 'Folder',
+                      datatype = 'File',
                       direction = 'Input',
-                      parameterType = 'Optional')]
-    #commonparams[1].value = r"C:\temp\geonislogs";
+                      parameterType = 'Optional'),
+                    Parameter(
+                      displayName = 'Log file',
+                      name = 'logfilepath',
+                      datatype = 'File',
+                      direction = 'Output',
+                      parameterType = 'Derived')]
     return commonparams
-    
+
+def getMultiDirInputParameter():
+    return Parameter(
+        displayName = "Input Directories",
+        name = "in_dirlist",
+        datatype = "Folder",
+        direction = "Input",
+        parameterType = "Optional",
+        multiValue = True
+        )
+
+def getMultiDirOutputParameter():
+    return Parameter(
+        displayName = "Output Directories",
+        name = "out_dirlist",
+        datatype = "Folder",
+        direction = "Output",
+        parameterType = "Derived",
+        multiValue = True
+        )
+
 def updateParametersCommon(parameters):
-    """ set a default log file path """
-    logpath = [p for p in parameters if p.name == 'logfilepath']
-    if len(logpath) == 1:
-        pass # esri already checks for existence of folder
-    return
+    """ common parameter update tasks. """
+    #not sure if there's anything to do here
+    #can't set warning messages here-do that in updateMessages
+    # get logpath parameter
+    #logpathfilter = [p for p in parameters if p.name == 'logfilepath']
+    # esri already checks for existence of folder
+    """
+    if logpathfilter and len(logpathfilter) > 0:
+        logpath = logpathfilter[0]
+        logpathvalue = logpath.valueAsText
+        if logpathvalue and logpathvalue != "":
+            if not os.path.isfile(os.path.join(logpathvalue,'geonis_wf.log')):
+                logpath.setWarningMessage("Log file does not exist and will be created as geonis_wf.log")
+            else:
+                logpath.setWarningMessage("Log file will be appended to.")
+    """
+    pass
 
 
-    
+
--- scripts/lno_geonis_wf.py
+++ scripts/lno_geonis_wf.py
@@ -12,76 +12,169 @@ import sys, os
 import arcpy
 
 from lno_geonis_base import ArcpyTool
-from lno_geonis_base import getListofCommonInputs, updateParametersCommon
+from lno_geonis_base import getListofCommonInputs, updateParametersCommon, getMultiDirInputParameter, getMultiDirOutputParameter
 
-arcpy.AddMessage("nothing")
 
-class testtool(ArcpyTool):
+class UnpackPackages(ArcpyTool):
     def __init__(self):
         pass
 
     @property
     def label(self):
-        return "testtool"
+        return "Unpack Packages"
 
     @property
     def alias(self):
-        return "testtool alias"
+        return "unpack"
 
     @property
     def description(self):
-        return "test description property of testtool, the original"
+        return "Finds packages in the input directory and unpacks them into separate directories, reads the EML, and downloads the spatial data. "
 
     def getParameterInfo(self):
         params = getListofCommonInputs()
+        self.numberCommonParams = len(params)
+        params.append(arcpy.Parameter(
+                        displayName = "Directory of Packages",
+                        name = "in_dir",
+                        datatype = "Folder",
+                        parameterType = "Required",
+                        direction = "Input"))
+        #for indexing
+        self.in_ndx = self.numberCommonParams
+        params.append(getMultiDirOutputParameter())
+        self.out_ndx = self.in_ndx + 1
         return params
 
     def updateParameters(self, parameters):
         updateParametersCommon(parameters)
+        parameters[0].value = True
         return
 
     def updateMessages(self, parameters):
+        #check for empty input dir
         return
 
     def execute(self, parameters, messages):
-        if(self.isRunningAsTool):
-            messages.AddMessage("this message brought to you via message object pass in")
+        showMsgs = parameters[0].value
+        if showMsgs and self.isRunningAsTool:
+            messages.AddMessage("About to create test dirs")
         else:
-            arcpy.AddMessage("this message brought to you via arcpy")
+            arcpy.AddMessage("Not running as tool.")
+        packageDir = parameters[self.in_ndx].valueAsText
+        if showMsgs:
+            messages.AddMessage("package dir is: " + str(os.path.isdir(packageDir)))
+        outputDir = os.path.abspath(os.path.join(os.path.join(packageDir,os.path.pardir),"workflow_dirs"))
+        if showMsgs:
+            messages.AddMessage(str(outputDir))
+        outDirList = []
+        for i in range(3):
+            newdir = os.path.join(outputDir, "pkg_" + str(i))
+            if not os.path.isdir(newdir):
+                os.mkdir(newdir)
+            outDirList.append(newdir)
+        arcpy.SetParameterAsText(self.out_ndx, ";".join(outDirList))
         return
 
 
-class test2tool(ArcpyTool):
+class CheckSpatialData(ArcpyTool):
     def __init__(self):
         pass
 
     @property
     def label(self):
-        return "test2tool"
+        return "Check Spatial Data"
 
     @property
     def alias(self):
-        return "test2tool alias"
+        return "runchecks"
 
     @property
     def description(self):
-        return "this is test 2 tool with changes"
+        return "Performs validation checks on the spatial data found in the input directories."
 
     def getParameterInfo(self):
         params = getListofCommonInputs()
+        self.numberCommonParams = len(params)
+        params.append(getMultiDirInputParameter())
+        self.in_ndx = self.numberCommonParams
+        params.append(getMultiDirOutputParameter())
+        self.out_ndx = self.in_ndx + 1
         return params
 
     def updateParameters(self, parameters):
+        parameters[0].value = True
         return
 
     def updateMessages(self, parameters):
         return
 
     def execute(self, parameters, messages):
-        if(self.isRunningAsTool):
-            messages.AddMessage("this message brought to you via message object pass in")
+        showMsgs = parameters[0].value
+        if parameters[self.in_ndx].value:
+            dirlist = parameters[self.in_ndx].valueAsText.split(';')
         else:
-            arcpy.AddMessage("this message brought to you via arcpy")
+            dirlist = []
+        if(showMsgs and self.isRunningAsTool):
+            messages.AddMessage("Now running checks...")
+            messages.AddMessage("number of dirs: " + str(len(dirlist)))
+        else:
+            arcpy.AddMessage("Testing checks...")
+        for d in dirlist:
+            messages.AddMessage("found d: " + d)
+            messages.AddMessage(str(os.path.isdir(d)))
+        #pass the list on
+        arcpy.SetParameterAsText(self.out_ndx, parameters[self.in_ndx].valueAsText)
+        return
+
+class CreateMetadata(ArcpyTool):
+    def __init__(self):
+        pass
+
+    @property
+    def label(self):
+        return "Create Metadata"
+
+    @property
+    def alias(self):
+        return "metadata"
+
+    @property
+    def description(self):
+        return "Creates new metadata from the EML."
+
+    def getParameterInfo(self):
+        params = getListofCommonInputs()
+        self.numberCommonParams = len(params)
+        params.append(getMultiDirInputParameter())
+        self.in_ndx = self.numberCommonParams
+        params.append(getMultiDirOutputParameter())
+        self.out_ndx = self.in_ndx + 1
+        return params
+
+    def updateParameters(self, parameters):
+        parameters[0].value = True
+        return
+
+    def updateMessages(self, parameters):
+        return
+
+    def execute(self, parameters, messages):
+        showMsgs = parameters[0].value
+        if parameters[self.in_ndx].value:
+            dirlist = parameters[self.in_ndx].valueAsText.split(';')
+        else:
+            dirlist = []
+        if(showMsgs and self.isRunningAsTool):
+            messages.AddMessage("Now creating metadata...")
+            messages.AddMessage("number of dirs: " + str(len(dirlist)))
+        else:
+            arcpy.AddMessage("Testing checks...")
+        for d in dirlist:
+            messages.AddMessage("found d: " + d)
+            messages.AddMessage(str(os.path.isdir(d)))
+        #pass the list on
+        arcpy.SetParameterAsText(self.out_ndx, parameters[self.in_ndx].valueAsText)
         return
 
 
