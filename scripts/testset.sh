6aa282cfdf44fbedb03e02032df123663c67013b
Jack Peterson <jack@tinybike.net>
2013-08-04 00:36:06 -0700

    added testset.sh shell script for automated testing of sites with spatial data

---
+++ scripts/testset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sites=( nwt and knz cap bnz pie )
+servers=( pasta-s pasta pasta pasta pasta-s pasta-s )
+
+for i in {0..5}
+do
+	python pasta2geonis.py --flush ${sites[i]}
+	python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
+done
