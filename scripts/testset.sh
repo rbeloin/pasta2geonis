fdaeceb1fce5704c4a81ad58e3bc043509b0fe7c
Jack Peterson <jack@tinybike.net>
2013-08-24 14:18:20 -0700

    added new fix for sddraft error in refreshmapservice

--- scripts/testset.sh
+++ scripts/testset.sh
@@ -4,6 +4,6 @@ servers=( pasta-s pasta pasta pasta pasta-s pasta-s pasta pasta-s )
 
 for i in {0..7}
 do
-    python pasta2geonis.py --flush ${sites[i]}
-    #python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
+    #python pasta2geonis.py --flush ${sites[i]}
+    python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
 done
