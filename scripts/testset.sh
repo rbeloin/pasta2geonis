fad80195679e19fef93dd1b4ae02b269ab454c57
Jack Peterson <jack@tinybike.net>
2013-08-26 00:37:17 -0700

    fixed error 001272 analyzer error in StageService_server (FINALLY)

--- scripts/testset.sh
+++ scripts/testset.sh
@@ -2,8 +2,8 @@
 sites=( nwt and knz cap pie bnz ntl vcr )
 servers=( pasta-s pasta pasta pasta pasta-s pasta-s pasta pasta-s )
 
-for i in {0..3}
+for i in {0..7}
 do
-    python pasta2geonis.py --flush ${sites[i]}
-    #python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
+    #python pasta2geonis.py --flush ${sites[i]}
+    python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
 done
