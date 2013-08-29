c9875a6262189c3e6c858ef6165a5177c0f05223
Jack Peterson <jack@tinybike.net>
2013-08-28 17:22:12 -0700

    fixed layer deletion bug

--- scripts/testset.sh
+++ scripts/testset.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-sites=( nwt and knz cap pie bnz ntl vcr )
-servers=( pasta-s pasta pasta pasta pasta-s pasta-s pasta-s pasta-s )
+sites=( nwt knz cap pie bnz vcr )
+servers=( pasta-s pasta pasta pasta-s pasta-s pasta-s )
 
 echo "python update_wf.py"
 echo ""
 python update_wf.py
 
-for i in {6..6}
+for i in {0..5}
 do
+    echo ""
     echo "python pasta2geonis.py --flush ${sites[i]}"
     echo ""
     python pasta2geonis.py --flush ${sites[i]}
+    echo ""
     echo "python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model"
     echo ""
     python pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
