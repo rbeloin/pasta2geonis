0cafd85d4ca0399af868aba8a487e1f99fa8acf6
Jack Peterson <jack@tinybike.net>
2013-12-18 12:27:14 -0800

    changed leftbar on the main map page to text-align left

--- scripts/testset.sh
+++ scripts/testset.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sites=( and nwt knz cap pie bnz vcr ntl luq sbc gce hbr mcr )
-servers=( pasta pasta-s pasta pasta pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s )
+#sites=( and nwt knz cap pie bnz vcr ntl luq sbc gce hbr mcr )
+sites=( and arc bes bnz cce cdr cap cwt fce gce hfr hbr jrn kbs knz luq mcm mcr nwt ntl pal pie sbc sev sgs vcr )
+#servers=( pasta pasta-s pasta pasta pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s pasta-s )
 
 echo "python /c/pasta2geonis/scripts/update_wf.py"
 echo ""
@@ -13,7 +14,9 @@ do
     echo ""
     python /c/pasta2geonis/scripts/pasta2geonis.py --flush ${sites[i]}
     echo ""
-    echo "python /c/pasta2geonis/scripts/pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model"
+    #echo "python /c/pasta2geonis/scripts/pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model"
+    echo "python /c/pasta2geonis/scripts/pasta2geonis.py -p pasta -s ${sites[i]} -i all --run-setup --run-model"
     echo ""
-    python /c/pasta2geonis/scripts/pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
+    #python /c/pasta2geonis/scripts/pasta2geonis.py -p ${servers[i]} -s ${sites[i]} -i all --run-setup --run-model
+    python /c/pasta2geonis/scripts/pasta2geonis.py -p pasta -s ${sites[i]} -i all --run-setup --run-model
 done
